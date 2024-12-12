0690a507ac9bb06536e4c95021ac008455b3e2d0
xchose31 <106611403+xchose31@users.noreply.github.com>
2024-12-12 18:27:30 +0300

    Update main.py
    fixed fourth print

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program!!!')
     print('And I change it every day')
     print("Let's check new idea!")
-    print("UFO changed sth!')
+    print("UFO changed sth!")
 
 
 if __name__ == '__main__':
